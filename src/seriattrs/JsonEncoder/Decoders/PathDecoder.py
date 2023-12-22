bdfdb2a1c00718367b37664f64318967866cf42c
Tesla2000 <fratajczak124@gmail.com>
2023-12-22 15:13:26 +0100

    Added Path encoder and decoder

--- src/seriattrs/JsonEncoder/Decoders/PathDecoder.py
+++ src/seriattrs/JsonEncoder/Decoders/PathDecoder.py
@@ -1,13 +1,14 @@
+from pathlib import Path
 from typing import Any
 
 from ...JsonEncoder import Decoder
 
 
-class BytesDecoder(Decoder):
+class PathDecoder(Decoder):
     @staticmethod
     def is_valid(element: Any) -> bool:
         return isinstance(element, str)
 
     @staticmethod
-    def decode(element: str, _) -> bytes:
-        return element.encode()
+    def decode(element: str, _) -> Path:
+        return Path(element)
